f547059971efc8e8a0d3388573cbf8e6e3e2ba6e
Vojtech Moravec <vojtech.moravec.st@vsb.cz>
2019-10-30 11:32:19 +0100

    Moved method to archive abstract class.

---
+++ src/main/java/quantization/de/shade/ILShadeSolver.java
@@ -0,0 +1,9 @@
+package quantization.de.shade;
+
+public class ILShadeSolver extends LShadeSolver {
+
+    public ILShadeSolver(int dimension, int populationSize, int generationCount, int memorySize) {
+        super(dimension, populationSize, generationCount, memorySize);
+    }
+
+}
