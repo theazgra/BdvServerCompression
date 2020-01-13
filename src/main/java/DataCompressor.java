5932c2c1ae6b20497ae9e1535f477db5b4e10eff
Vojtech Moravec <vojtech.moravec.st@vsb.cz>
2020-01-13 18:49:54 +0100

    Improved CLI interface, allow multiple input files.

--- src/main/java/DataCompressor.java
+++ src/main/java/DataCompressor.java
@@ -1,6 +1,8 @@
+import cli.ImprovedOptionGroup;
 import org.apache.commons.cli.*;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class DataCompressor {
 
@@ -13,8 +15,8 @@ public class DataCompressor {
     private static final String BITS_SHORT = "b";
     private static final String BITS_LONG = "bits";
 
-    private static final String INPUT_SHORT = "i";
-    private static final String INPUT_LONG = "input";
+    //    private static final String INPUT_SHORT = "i";
+    //    private static final String INPUT_LONG = "input";
 
     private static final String OUTPUT_SHORT = "o";
     private static final String OUTPUT_LONG = "output";
@@ -24,7 +26,7 @@ public class DataCompressor {
 
         Options options = new Options();
 
-        OptionGroup methodGroup = new OptionGroup();
+        OptionGroup methodGroup = new ImprovedOptionGroup();
 
         methodGroup.setRequired(true);
         methodGroup.addOption(new Option(COMPRESS_SHORT, COMPRESS_LONG, false, "Compress 16 bit raw image"));
@@ -33,8 +35,8 @@ public class DataCompressor {
 
         options.addOptionGroup(methodGroup);
         options.addOption(BITS_SHORT, BITS_LONG, true, "Bit count per pixel [Default 8]");
-        options.addRequiredOption(INPUT_SHORT, INPUT_LONG, true, "Input file");
-        options.addRequiredOption(OUTPUT_SHORT, OUTPUT_LONG, true, "Output file");
+        //        options.addRequiredOption(INPUT_SHORT, INPUT_LONG, true, "Input file");
+        options.addOption(OUTPUT_SHORT, OUTPUT_LONG, true, "Custom output directory");
 
         HelpFormatter formatter = new HelpFormatter();
 
@@ -44,7 +46,7 @@ public class DataCompressor {
         try {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
-            if (args[0].equals("-h") || args[0].equals("--help")) {
+            if ((args.length > 0) && (args[0].equals("-h") || args[0].equals("--help"))) {
                 formatter.printHelp("ijava -jar DataCompressor.jar", options);
                 return;
             }
@@ -57,25 +59,27 @@ public class DataCompressor {
             return;
         }
 
-        final String bitsString = cmd.getOptionValue(BITS_LONG, "8");
-        final int bits = Integer.parseInt(bitsString);
+        // Default output directory to current directory.
+        final String currentOutputDirectory = cmd.getOptionValue(OUTPUT_LONG,
+                                                                 Paths.get("").toAbsolutePath().toString());
+
+        // Default bit count per pixel to 8.
+        final int bits = Integer.parseInt(cmd.getOptionValue(BITS_LONG, "8"));
+
+        // Files to compress decompress
+        final String[] files = cmd.getArgs();
+        System.out.println("Output directory: " + currentOutputDirectory);
+        System.out.println("Bit count: " + bits);
 
         if (cmd.hasOption(COMPRESS_SHORT) || cmd.hasOption(COMPRESS_LONG)) {
-            System.out.println(String.format("Compressing %s with %d bits, saving to %s",
-                                             cmd.getOptionValue(INPUT_SHORT),
-                                             bits,
-                                             cmd.getOptionValue(OUTPUT_SHORT)));
+            System.out.println("Compressing: ...");
 
             // TODO(Moravec): Do compression here.
             return;
         }
 
         if (cmd.hasOption(DECOMPRESS_SHORT) || cmd.hasOption(DECOMPRESS_LONG)) {
-            System.out.println(String.format("Decompressing %s with %d bits, saving to %s",
-                                             cmd.getOptionValue(INPUT_SHORT),
-                                             bits,
-                                             cmd.getOptionValue(OUTPUT_SHORT)));
-
+            System.out.println("Decompressing: ...");
             // TODO(Moravec): Do decompression here.
             return;
         }
