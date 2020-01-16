762996f963e7d49299a37e16946ab7548b26ca59
Vojtech Moravec <vojtech.moravec.st@vsb.cz>
2020-01-16 13:39:50 +0100

    Write,Read of QCMPFile header.

--- src/main/java/compression/fileformat/QCMPFileHeader.java
+++ src/main/java/compression/fileformat/QCMPFileHeader.java
@@ -2,10 +2,14 @@ package compression.fileformat;
 
 import compression.U16;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class QCMPFileHeader {
     public static final String QCMP_MAGIC_VALUE = "QCMPFILE";
 
-    private String magicValue;
+    private String magicValue = QCMP_MAGIC_VALUE;
     private QuantizationType quantizationType;
     private byte bitsPerPixel;
     private boolean codebookPerPlane;
@@ -18,8 +22,10 @@ public class QCMPFileHeader {
     private int vectorSizeY;
     private int vectorSizeZ;
 
+
     /**
      * Validate that all header values are in their valid range.
+     *
      * @return True if this is valid QCMPFILE header.
      */
     public boolean validateHeader() {
@@ -44,5 +50,37 @@ public class QCMPFileHeader {
             return false;
 
         return true;
+    }
+
+    private void writeHeader(DataOutputStream outputStream) throws IOException {
+        outputStream.writeBytes(QCMP_MAGIC_VALUE);
+
+        outputStream.writeByte(quantizationType.getValue());
+        outputStream.writeByte(bitsPerPixel);
+        outputStream.writeBoolean(codebookPerPlane);
+
+        outputStream.writeInt(imageSizeX);
+        outputStream.writeInt(imageSizeY);
+        outputStream.writeInt(imageSizeZ);
+
+        outputStream.writeInt(vectorSizeX);
+        outputStream.writeInt(vectorSizeY);
+        outputStream.writeInt(vectorSizeZ);
+    }
+
+    private void readHeader(DataInputStream inputStream) throws IOException {
+        magicValue = new String(inputStream.readNBytes(8));
+
+        quantizationType = QuantizationType.fromByte(inputStream.readByte());
+        bitsPerPixel = inputStream.readByte();
+        codebookPerPlane = inputStream.readBoolean();
+
+        imageSizeX = inputStream.readInt();
+        imageSizeY = inputStream.readInt();
+        imageSizeZ = inputStream.readInt();
+
+        vectorSizeX = inputStream.readInt();
+        vectorSizeY = inputStream.readInt();
+        vectorSizeZ = inputStream.readInt();
     }
 }
--- src/main/java/compression/fileformat/QuantizationType.java
+++ src/main/java/compression/fileformat/QuantizationType.java
@@ -1,7 +1,7 @@
 package compression.fileformat;
 
 public enum QuantizationType {
-    Scalar(0), Vector1D(1), Vector2D(2);
+    Scalar(0), Vector1D(1), Vector2D(2), Vector3D(3), Invalid(255);
 
     private final int value;
 
@@ -11,5 +11,18 @@ public enum QuantizationType {
 
     public int getValue() {
         return value;
+    }
+
+    public static QuantizationType fromByte(final byte b) {
+        if (b == 0)
+            return Scalar;
+        if (b == 1)
+            return Vector1D;
+        if (b == 2)
+            return Vector2D;
+        if (b == 3)
+            return Vector3D;
+        else
+            return Invalid;
     }
 }
