c067942d23245690ac2fd55c61f9f44bf9944f7b
Vojtech Moravec <vojtech.moravec.st@vsb.cz>
2020-01-22 09:40:40 +0100

    Reworked cache to use DataStreams and TryWithResource

--- src/main/java/azgracompress/quantization/QuantizationValueCache.java
+++ src/main/java/azgracompress/quantization/QuantizationValueCache.java
@@ -15,7 +15,7 @@ public class QuantizationValueCache {
 
     private File getCacheFileForScalarValues(final String trainFile, final int quantizationValueCount) {
         final File cacheFile = new File(cacheFolder, String.format("%s_%d_bits.qvc",
-                trainFile, quantizationValueCount));
+                                                                   trainFile, quantizationValueCount));
         return cacheFile;
     }
 
@@ -24,24 +24,23 @@ public class QuantizationValueCache {
                                              final int entryWidth,
                                              final int entryHeight) {
         final File cacheFile = new File(cacheFolder, String.format("%s_%d_%dx%d.qvc",
-                trainFile, codebookSize, entryWidth, entryHeight));
+                                                                   trainFile, codebookSize, entryWidth, entryHeight));
         return cacheFile;
     }
 
-    public void saveQuantizationValue(final String trainFile, final int[] quantizationValues) {
+    public void saveQuantizationValues(final String trainFile, final int[] quantizationValues) {
         final int quantizationValueCount = quantizationValues.length;
         final String cacheFile = getCacheFileForScalarValues(trainFile, quantizationValueCount).getAbsolutePath();
 
+        try (FileOutputStream fos = new FileOutputStream(cacheFile, false);
+             DataOutputStream dos = new DataOutputStream(fos)) {
 
-        try {
-            // NOTE(Moravec): RandomAccessFile is slow but provides binary APIs.
-            RandomAccessFile rac = new RandomAccessFile(cacheFile, "rw");
             for (final int qv : quantizationValues) {
-                rac.writeInt(qv);
+                dos.writeInt(qv);
             }
-            rac.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException ioEx) {
+            System.err.println("Failed to save scalar quantization values to cache.");
+            ioEx.printStackTrace();
         }
     }
 
@@ -49,24 +48,46 @@ public class QuantizationValueCache {
         final int codebookSize = entries.length;
         final int entryWidth = entries[0].getWidth();
         final int entryHeight = entries[0].getHeight();
-        final String cacheFile = getCacheFileForVectorValues(trainFile, codebookSize, entryWidth, entryHeight).getAbsolutePath();
+        final String cacheFile = getCacheFileForVectorValues(trainFile,
+                                                             codebookSize,
+                                                             entryWidth,
+                                                             entryHeight).getAbsolutePath();
 
 
-        try {
-            // NOTE(Moravec): RandomAccessFile is slow but provides binary APIs.
-            RandomAccessFile rac = new RandomAccessFile(cacheFile, "rw");
-            rac.writeInt(codebookSize);
-            rac.writeInt(entryWidth);
-            rac.writeInt(entryHeight);
+        try (FileOutputStream fos = new FileOutputStream(cacheFile, false);
+             DataOutputStream dos = new DataOutputStream(fos)) {
+
+            dos.writeInt(codebookSize);
+            dos.writeInt(entryWidth);
+            dos.writeInt(entryHeight);
 
             for (final CodebookEntry entry : entries) {
                 for (final int vectorValue : entry.getVector()) {
-                    rac.writeInt(vectorValue);
+                    dos.writeInt(vectorValue);
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException ioEx) {
+            System.err.println("Failed to save quantization vectors to cache.");
+            ioEx.printStackTrace();
         }
+    }
+
+    public int[] readCachedValues(final String trainFile, final int quantizationValueCount) {
+        final File cacheFile = getCacheFileForScalarValues(trainFile, quantizationValueCount);
+
+        int[] values = new int[quantizationValueCount];
+        try (FileInputStream fis = new FileInputStream(cacheFile);
+             DataInputStream dis = new DataInputStream(fis)) {
+
+            for (int i = 0; i < quantizationValueCount; i++) {
+                values[i] = dis.readInt();
+            }
+        } catch (IOException ioEx) {
+            System.err.println("Failed to read scalar quantization values from cache.");
+            ioEx.printStackTrace();
+            return new int[0];
+        }
+        return values;
     }
 
     public CodebookEntry[] readCachedValues(final String trainFile,
@@ -74,32 +95,33 @@ public class QuantizationValueCache {
                                             final int entryWidth,
                                             final int entryHeight) {
         final File cacheFile = getCacheFileForVectorValues(trainFile, codebookSize, entryWidth, entryHeight);
-        try {
-            // NOTE(Moravec): RandomAccessFile is slow but provides binary APIs.
-            RandomAccessFile rac = new RandomAccessFile(cacheFile, "r");
 
-            final int savedCodebookSize = rac.readInt();
-            final int savedEntryWidth = rac.readInt();
-            final int savedEntryHeight = rac.readInt();
+        CodebookEntry[] codebook = new CodebookEntry[codebookSize];
+        try (FileInputStream fis = new FileInputStream(cacheFile);
+             DataInputStream dis = new DataInputStream(fis)) {
+
+            final int savedCodebookSize = dis.readInt();
+            final int savedEntryWidth = dis.readInt();
+            final int savedEntryHeight = dis.readInt();
             assert (savedCodebookSize == codebookSize) : "Wrong codebook size";
             assert (savedEntryWidth == entryWidth) : "Wrong entry width";
             assert (savedEntryHeight == entryHeight) : "Wrong entry height";
 
-            CodebookEntry[] codebook = new CodebookEntry[codebookSize];
             final int entrySize = entryWidth * entryHeight;
             for (int i = 0; i < codebookSize; i++) {
                 int[] vector = new int[entrySize];
                 for (int j = 0; j < entrySize; j++) {
-                    vector[j] = rac.readInt();
+                    vector[j] = dis.readInt();
                 }
                 codebook[i] = new CodebookEntry(vector);
             }
-
-            return codebook;
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException ioEx) {
+            System.err.println("Failed to read quantization vectors from cache.");
+            ioEx.printStackTrace();
+            return new CodebookEntry[0];
         }
-        return new CodebookEntry[0];
+        return codebook;
+
     }
 
     public boolean areQuantizationValueCached(final String trainFile, final int quantizationValueCount) {
@@ -115,19 +137,5 @@ public class QuantizationValueCache {
         return cacheFile.exists();
     }
 
-    public int[] readCachedValues(final String trainFile, final int quantizationValueCount) {
-        final File cacheFile = getCacheFileForScalarValues(trainFile, quantizationValueCount);
-        try {
-            // NOTE(Moravec): RandomAccessFile is slow but provides binary APIs.
-            RandomAccessFile rac = new RandomAccessFile(cacheFile, "r");
-            int[] values = new int[quantizationValueCount];
-            for (int i = 0; i < quantizationValueCount; i++) {
-                values[i] = rac.readInt();
-            }
-            return values;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new int[0];
-    }
+
 }
