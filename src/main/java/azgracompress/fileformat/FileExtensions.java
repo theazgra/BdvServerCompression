3f183cf80d4373a490d5c6531356021b9185edd5
Vojtech Moravec <vojtech.moravec.st@vsb.cz>
2020-02-14 20:25:41 +0100

    Added QCMP file constants.

--- src/main/java/azgracompress/fileformat/FileExtensions.java
+++ src/main/java/azgracompress/fileformat/FileExtensions.java
@@ -3,4 +3,6 @@ package azgracompress.fileformat;
 public class FileExtensions {
     public static final String RAW = "raw";
     public static final String TIFF = "tiff";
+    public static final String QCMP = "qcmp";
+
 }
---
+++ src/main/java/azgracompress/fileformat/FileType.java
@@ -0,0 +1,7 @@
+package azgracompress.fileformat;
+
+public enum FileType {
+    RAW,
+    TIFF,
+    QCMP
+}
